25542ad1cb96dbc83262c34d78e82ef6ace5ed3e
RobertoDelCampo <robertogdelc@gmail.com>
2023-06-28 12:59:15 -0600

    update

--- pages/api/alexa.js
+++ pages/api/alexa.js
@@ -75,7 +75,7 @@ export default function handler(req, res) {
             type: "PlainText",
             text: text,
           },
-          shouldEndSession: remainSession,
+          shouldEndSession: !remainSession,
         },
       });
     } catch (error) {
