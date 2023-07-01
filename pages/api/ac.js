25bbe641a76d7ab00501170acd011e4a7607d8a7
RobertoDelCampo <robertogdelc@gmail.com>
2023-07-01 16:05:42 -0600

    Update

--- pages/api/ac.js
+++ pages/api/ac.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+    console.log(`Received a request: ${req}`)
     if (req.method === 'POST') {
         var { request } = req.body
         const data = JSON.stringify(request, null, 2)
 
         console.log(`Received POST: \n${data}`)
-        
+        res.status(200).json({
+            version: "1.0",
+            response: {
+              outputSpeech: {
+                type: "PlainText",
+                text: "Hello",
+              },
+              shouldEndSession: true,
+            }
+        })
+    }
+    else {
+        res.status(400).json({ error: 'This endpoint requires a POST request.' })
     }
 }
 
