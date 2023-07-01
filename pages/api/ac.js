b3a835553182566e890969ef32eab4ef86bc5ebd
RobertoDelCampo <robertogdelc@gmail.com>
2023-07-01 16:15:30 -0600

    s

--- pages/api/ac.js
+++ pages/api/ac.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     console.log(`Received a request: ${req.method}`)
     if (req.method === 'POST') {
         try{
-            const data = JSON.stringify(req.body.request, null, 2)
+            const data = JSON.stringify(req.body, null, 2)
             console.log(`Received POST: \n${data}`)
             res.status(200).json({
                 version: "1.0",
